5f3cd59e818f6258da5c21abfb31e0e206d2d407
torshin5ergey <ttorshin5ergey@gmail.com>
2025-05-30 17:31:54 +0300

    add target directory for archive

--- log-archive.sh
+++ log-archive.sh
@@ -2,15 +2,22 @@
 
 set -e
 
+# Check source directory is not empty
 if [ -z "$1" ]; then
-  echo "Please, provide path to directory for archiving."
+  echo "Please, provide path to archived directory."
   exit 1
+fi
+
+if [ -n "$2" ]; then
+  TARGET_DIR=$2
+else
+  TARGET_DIR="./"
 fi
 
 SOURCE_DIR=$1
 DATETIME=$(date +"%Y%m%d_%H%M%S")
 ARCHIVE_NAME="logs_archive_$DATETIME.tar.gz"
 
-tar cfz $ARCHIVE_NAME $SOURCE_DIR
+tar cfz $TARGET_DIR/$ARCHIVE_NAME $SOURCE_DIR
 
-echo "Archived to $ARCHIVE_NAME"
+echo "Archived to $TARGET_DIR/$ARCHIVE_NAME"

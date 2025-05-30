e6dd4d9bb81131ff193c65aa63b7ab7a43a1e2e1
torshin5ergey <ttorshin5ergey@gmail.com>
2025-05-30 18:13:01 +0300

    add help

--- log-archive.sh
+++ log-archive.sh
@@ -2,22 +2,40 @@
 
 set -e
 
+# Display help
+show_help() {
+  echo "log-archive - simple archive tool"
+  echo -e "\nUsage:"
+  echo "  log-archive [-h] SOURCE [TARGET_DIR]"
+  echo -e "\noptional arguments:"
+  echo "  TARGET_DIR   target directory for archive"
+  echo -e "  -h, --help   show this help message and exit\n"
+}
+
 # Check source directory is not empty
 if [ -z "$1" ]; then
   echo "Please, provide path to archived directory."
+  show_help
   exit 1
 fi
 
+# Show help
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+  show_help
+  exit 0
+fi
+
+# Get target directory
 if [ -n "$2" ]; then
-  TARGET_DIR=$2
+  TARGET_DIR="$2"
 else
   TARGET_DIR="./"
 fi
 
-SOURCE_DIR=$1
+SOURCE_DIR="$1"
 DATETIME=$(date +"%Y%m%d_%H%M%S")
-ARCHIVE_PATH=${TARGET_DIR%/}/"logs_archive_$DATETIME.tar.gz"
+ARCHIVE_PATH="${TARGET_DIR%/}/logs_archive_$DATETIME.tar.gz"
 
-tar cfz $ARCHIVE_PATH $SOURCE_DIR
+tar cfz "$ARCHIVE_PATH" "$SOURCE_DIR"
 
 echo "Successfully archived to $ARCHIVE_PATH"
